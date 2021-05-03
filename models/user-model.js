9d1931ed209939c744d55986953337036ba663b4
CullenSharp2 <cullen.sharp100@gmail.com>
2021-05-03 15:09:44 -0700

    Modulurarize userModel

---
+++ models/user-model.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const toolSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  category: {type: String, required: true},
+  owner: {type: String, required: true},
+  borrowedBy: {type: String},
+  Availbility: {type: Boolean, required: true},
+})
+
+const userSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String},
+  location: {type: Array, required: true},
+  rating: {type: Number},
+  tools: [toolSchema]
+})
+
+const UserModel = new mongoose.model('User', userSchema);
+
+exports.User = UserModel;
