5341bb19f6e342ced0d1ad32b22a5b2d0bc0a3a8
Dion Johnson <dionjwa@gmail.com>
2021-05-03 18:41:16 -0700

    Dion Work

---
+++ src/data.js
@@ -0,0 +1,49 @@
+'use strict';
+const User = require('../models/user-model');
+
+const data = {};
+
+data.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+}
+
+data.addTool = async (req, res) => {
+  const _id = req.params.id;
+  const {tools} = req.body;
+
+  try{
+    await User.find({_id}, (err, users) => {
+      if(!users.length) console.log('no user found');
+
+      const user = users[0];
+      user.tools.push(tools[0]);
+      user.save();
+
+      res.status(200).json(user);
+  })
+  } catch (err) {
+    res.status(400).json({message: err.message});
+  }
+}
+
+data.deleteTool = async (req, res) => {
+  const _id = req.params.id;
+  console.log({_id});
+  try{
+    
+    await Promise.all([(User.find({_id})),(User.deleteOne({_id}))]);
+   
+
+    res.status(200).json('Did it');
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+}
+
+
+module.exports = data;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('../models/user-model');
 const PORT = process.env.PORT || 3002;
+const data = require('./data');
 
 const app = express();
 app.use(cors());
@@ -21,38 +22,10 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {console.log('Mongoose is connected')});
 
+//Routes//
+app.delete('/users/:id', data.deleteTool);
+app.get('/users', data.getAllUsers);
 
-// app.get('*', (req, res) => {
-//   res.send()
-// })
-
-//sends back all users in collection
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({message: err.message});
-  }
-})
-
-app.post('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-  const {tools} = req.body;
-
-  try{
-    await User.find({_id}, (err, users) => {
-      if(!users.length) console.log('no user found');
-
-      const user = users[0];
-      user.tools.push(tools[0]);
-      user.save();
-
-      res.status(200).json(user);
-  })
-  } catch (err) {
-    res.status(400).json({message: err.message})
-  }
-})
+app.post('/users/:id', data.addTool);
 
 app.listen(PORT, () => {console.log(`Getting jiggy with it at ${PORT}.`);})
